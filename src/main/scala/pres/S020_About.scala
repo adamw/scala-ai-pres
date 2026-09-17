64166d8af17a1cf25b7df19448b44c3acb2ad816
adamw <adam@warski.org>
2026-09-17 10:30:47 +0200

    WIP

---
+++ src/main/scala/pres/S020_About.scala
@@ -0,0 +1,28 @@
+package pres
+
+class S020_About:
+  // * Head of R&D at VirtusLab
+  // * SoftwareMill Division
+  // * OpenSource: Tapir, sttp, Ox, Jox, Envers, ElasticMQ, Orca
+  // * Blogging & speaking: https://warski.org
+
+  def VirtusLab() =
+    val Scala = true
+    val Java = true
+    val Kotlin = true
+
+    val AI = true
+
+    val Kafka = true
+
+    val DeveloperTooling = true
+    val Bazel = true
+
+    val DistributedSystems = true
+    val Data = true
+    val Backends = true
+
+    val _and_more = true
+
+  // We're trying to make sense of all the new AI tooling as well:
+  // Visdom: https://visdom.virtuslab.com
